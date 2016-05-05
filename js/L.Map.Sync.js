5804ebef2411c6545fadb1d8a8a04b3306f95fe3
justingreco <justin.greco@raleighnc.gov>
2016-05-05 14:04:16 -0400

    textual changes

--- js/L.Map.Sync.js
+++ js/L.Map.Sync.js
@@ -5,34 +5,23 @@
 (function () {
     'use strict';
 
+    var NO_ANIMATION =  {
+        animate: false,
+        reset: true
+    };
+
     L.Map = L.Map.extend({
         sync: function (map, options) {
             this._initSync();
             options = options || {};
 
             // prevent double-syncing the map:
-            var present = false;
-            // this._syncMaps.forEach(function (other) {
-            //     if (map === other) {
-            //         present = true;
-            //     }
-            // });
-
-            $(this._syncMaps).each(function (i, other) {
-              if (map === other) {
-                  present = true;
-              }
-            });
-
-            if (!present) {
+            if (this._syncMaps.indexOf(map) === -1) {
                 this._syncMaps.push(map);
             }
 
             if (!options.noInitialSync) {
-                map.setView(this.getCenter(), this.getZoom(), {
-                    animate: false,
-                    reset: true
-                });
+                map.setView(this.getCenter(), this.getZoom(), NO_ANIMATION);
             }
             return this;
         },
@@ -42,22 +31,22 @@
             var self = this;
 
             if (this._syncMaps) {
-                // this._syncMaps.forEach(function (synced, id) {
-                //     if (map === synced) {
-                //         self._syncMaps.splice(id, 1);
-                //     }
-                // });
-                $(this._syncMaps).each(function (id, synced) {
-                  if (map === synced) {
-                      self._syncMaps.splice(id, 1);
-                  }
+                this._syncMaps.forEach(function (synced, id) {
+                    if (map === synced) {
+                        self._syncMaps.splice(id, 1);
+                    }
                 });
             }
 
             return this;
         },
 
-        // overload methods on originalMap to replay on _syncMaps;
+        // Checks if the maps is synced with anything
+        isSynced: function () {
+            return (this.hasOwnProperty('_syncMaps') && Object.keys(this._syncMaps).length > 0);
+        },
+
+        // overload methods on originalMap to replay interactions on _syncMaps;
         _initSync: function () {
             if (this._syncMaps) {
                 return;
@@ -69,12 +58,8 @@
             L.extend(originalMap, {
                 setView: function (center, zoom, options, sync) {
                     if (!sync) {
-                        // originalMap._syncMaps.forEach(function (toSync) {
-                        //     toSync.setView(center, zoom, options, true);
-                        // });
-
-                        $(originalMap._syncMaps).each(function (i, toSync) {
-                          toSync.setView(center, zoom, options, true);
+                        originalMap._syncMaps.forEach(function (toSync) {
+                            toSync.setView(center, zoom, options, true);
                         });
                     }
                     return L.Map.prototype.setView.call(this, center, zoom, options);
@@ -82,11 +67,8 @@
 
                 panBy: function (offset, options, sync) {
                     if (!sync) {
-                        // originalMap._syncMaps.forEach(function (toSync) {
-                        //     toSync.panBy(offset, options, true);
-                        // });
-                        $(originalMap._syncMaps).each(function (i, toSync) {
-                          toSync.panBy(offset, options, true);
+                        originalMap._syncMaps.forEach(function (toSync) {
+                            toSync.panBy(offset, options, true);
                         });
                     }
                     return L.Map.prototype.panBy.call(this, offset, options);
@@ -94,11 +76,8 @@
 
                 _onResize: function (event, sync) {
                     if (!sync) {
-                        // originalMap._syncMaps.forEach(function (toSync) {
-                        //     toSync._onResize(event, true);
-                        // });
-                        $(originalMap._syncMaps).each(function (i, toSync) {
-                          toSync._onResize(event, true);
+                        originalMap._syncMaps.forEach(function (toSync) {
+                            toSync._onResize(event, true);
                         });
                     }
                     return L.Map.prototype._onResize.call(this, event);
@@ -106,32 +85,23 @@
             });
 
             originalMap.on('zoomend', function () {
-                // originalMap._syncMaps.forEach(function (toSync) {
-                //     toSync.setView(originalMap.getCenter(), originalMap.getZoom(), {
-                //         animate: false,
-                //         reset: false
-                //     });
-                // });
-                $(originalMap._syncMaps).each(function (i, toSync) {
-                  toSync.setView(originalMap.getCenter(), originalMap.getZoom(), {
-                      animate: false,
-                      reset: false
-                  });
+                originalMap._syncMaps.forEach(function (toSync) {
+                    toSync.setView(originalMap.getCenter(), originalMap.getZoom(), NO_ANIMATION);
                 });
             }, this);
 
             originalMap.dragging._draggable._updatePosition = function () {
                 L.Draggable.prototype._updatePosition.call(this);
                 var self = this;
-                // originalMap._syncMaps.forEach(function (toSync) {
-                //     L.DomUtil.setPosition(toSync.dragging._draggable._element, self._newPos);
-                //     toSync.fire('moveend');
-                // });
-                $(originalMap._syncMaps).each(function (i, toSync) {
-                  L.DomUtil.setPosition(toSync.dragging._draggable._element, self._newPos);
-                  toSync.fire('moveend');
+                originalMap._syncMaps.forEach(function (toSync) {
+                    L.DomUtil.setPosition(toSync.dragging._draggable._element, self._newPos);
+                    toSync.eachLayer(function (layer) {
+                        if (layer._google !== undefined) {
+                            layer._google.setCenter(originalMap.getCenter());
+                        }
+                    });
+                    toSync.fire('moveend');
                 });
-
             };
         }
     });
